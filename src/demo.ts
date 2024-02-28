9cf70cb53227d6cc3a7ac4c15c32ec60ad1ad1c4
Riccardo Spadon <spadon.riccardo@gmail.com>
2024-02-28 17:03:27 +0100

    TypeScript Chapter 4
    - Updates;

---
+++ src/demo.ts
@@ -0,0 +1,62 @@
+// part 1 
+let x: Record<string, string | number | boolean | Function> = { name: "Wruce Bayne" }
+x.number = 1234
+x.active = true
+x.log = () => console.log("awesome!")
+
+
+////////////////////
+
+type ContactStatus = "active" | "inactive" | "new";
+
+interface Address {
+    street: string;
+    province: string;
+    postalCode: string;
+}
+
+interface Contact {
+    id: number;
+    name: string;
+    status: ContactStatus;
+    address: Address;
+    email: string;
+}
+
+interface Query<TProp> {
+    sort?: 'asc' | 'desc';
+    matches(val: TProp): boolean;
+}
+
+// commentato per la parte 2
+// type ContactQuery = Partial<<Record<keyof Contact, Query>>>
+
+// parte 2
+type ContactQuery = {
+    [TProp in keyof Contact]?: Query<Contact[TProp]>
+}
+
+type RequiredContactQuery = Required<ContactQuery>
+
+function searchContacts(contacts: Contact[], query: ContactQuery) {
+    return contacts.filter(contact => {
+        for (const property of Object.keys(contact) as (keyof Contact)[]) {
+            // get the query object for this property
+            const propertyQuery = query[property] as Query<Contact[keyof Contact]>;
+            // check to see if it matches
+            if (propertyQuery && propertyQuery.matches(contact[property])) {
+                return true;
+            }
+        }
+
+        return false;
+    })
+}
+
+const filteredContacts = searchContacts(
+    [/* contacts */],
+    {
+        id: { matches: (id) => id === 123 },
+        name: { matches: (name) => name === "Carol Weaver" },
+    }
+);
